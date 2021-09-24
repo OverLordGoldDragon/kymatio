2c48480e4c12732c0bc4927e73815374c43421f7
OverLordGoldDragon <16495490+OverLordGoldDragon@users.noreply.github.com>
2021-09-24 23:43:05 +0400

    Update jtfs_echirp.py

--- examples/1d/jtfs_echirp.py
+++ examples/1d/jtfs_echirp.py
@@ -11,12 +11,11 @@ FDTS-(frequency-dependent time shifts) discriminability.
 # Import the necessary packages
 # -----------------------------
 from kymatio.numpy import TimeFrequencyScattering1D
-from kymatio import visuals
-from kymatio.visuals import plot, imshow
 from kymatio.toolkit import echirp
-import numpy as np
+from kymatio.visuals import plot, imshow
+from kymatio import visuals
 
-#%%# Generate echirp and create scattering object #############################
+#%% Generate echirp and create scattering object #############################
 N = 4096
 # span low to Nyquist; assume duration of 1 second
 x = echirp(N, fmin=64, fmax=N/2)
@@ -26,7 +25,7 @@ x = echirp(N, fmin=64, fmax=N/2)
 J = 9
 # 8 bandpass wavelets per octave
 # J*Q ~= 72 total temporal coefficients in first-order scattering
-Q = 8
+Q = 16  # TODO
 # scale of temporal invariance, 31.25 ms
 T = 2**7
 # 4 frequential octaves
@@ -34,22 +33,26 @@ J_fr = 4
 # 2 bandpass wavelets per octave
 Q_fr = 2
 # scale of frequential invariance, F/Q == 1 cycle per octave
-F = 8
+F = 4
 # do frequential averaging to enable 4D concatenation
 average_fr = True
+# frequential padding; 'zero' avoids few discretization artefacts for this example
+pad_mode_fr = 'zero'
 # return packed as dict keyed by pair names for easy inspection
 out_type = 'dict:array'
 
-jtfs = TimeFrequencyScattering1D(shape=N, J=J, Q=Q, T=T, J_fr=J_fr, Q_fr=Q_fr,
-                                 F=F, average_fr=average_fr, out_type=out_type)
+params = dict(J=J, Q=Q, T=T, J_fr=J_fr, Q_fr=Q_fr, F=F, average_fr=average_fr,
+              out_type=out_type, pad_mode_fr=pad_mode_fr, max_pad_factor=4,
+              max_pad_factor_fr=4)
+jtfs = TimeFrequencyScattering1D(shape=N, **params)
 
-#%%# Take JTFS, print pair names and shapes ###################################
+#%% Take JTFS, print pair names and shapes ###################################
 Scx = jtfs(x)
 print("JTFS pairs:")
 for pair in Scx:
     print("{:<12} -- {}".format(str(Scx[pair].shape), pair))
 
-#%%# Show `x` and its (time-averaged) scalogram ###############################
+#%% Show `x` and its (time-averaged) scalogram ###############################
 plot(x, show=1,
      xlabel="time [samples]",
      title="Exponential chirp | fmin=64, fmax=2048, 4096 samples")
@@ -57,3 +60,20 @@ imshow(Scx['S1'].squeeze(), abs=1,
        xlabel="time [samples] (subsampled)",
        ylabel="frequency [Hz]",
        title="Scalogram, time-averaged (first-order scattering)")
+
+#%% Create & save GIF ########################################################
+# fetch meta (structural info)
+jmeta = jtfs.meta()
+# specify save folder
+savedir = ''
+visuals.gif_jtfs(Scx, jmeta, savedir=savedir, save_images=0, overwrite=1)
+# Notice how -1 spin coefficients contain nearly all the energy
+# and +1 barely any; this is FDTS discriminability.
+# For ideal FDTS and JTFS, +1 will be all zeros.
+# For down chirp, the case is reversed and +1 hoards the energy.
+
+#%% Show joint wavelets on a nicer filterbank ################################
+jtfs_min = TimeFrequencyScattering1D(shape=512, J=5, Q=16, J_fr=3, Q_fr=1)
+# nearly invisible, omit (also unused in scattering per `j2 > 0`)
+jtfs_min.psi2_f.pop(0)
+visuals.filterbank_jtfs(jtfs_min, part='real')
